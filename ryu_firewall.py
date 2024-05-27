10fbf79162ee62966bff0ecaa74b76a635d2ecf6
ramziwashere <143566346+ramziwashere@users.noreply.github.com>
2024-05-27 11:28:50 +1000

    Add files via upload

--- ryu_firewall.py
+++ ryu_firewall.py
@@ -1,13 +1,27 @@
+# Copyright (C) 2011 Nippon Telegraph and Telephone Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from ryu.base import app_manager
 from ryu.controller import ofp_event
 from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
 from ryu.lib.packet import packet
-from ryu.lib.packet import ethernet
+from ryu.lib.packet import ethernet,tcp,ipv4,udp
 from ryu.lib.packet import ether_types
-from ryu.lib.packet import ipv4
-from ryu.lib.packet import tcp
+
 
 class SimpleSwitch13(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
@@ -15,15 +29,6 @@ class SimpleSwitch13(app_manager.RyuApp):
     def __init__(self, *args, **kwargs):
         super(SimpleSwitch13, self).__init__(*args, **kwargs)
         self.mac_to_port = {}
-        self.ip_to_port = {
-            '192.168.1.10': 1,  # PC1
-            '192.168.1.11': 2,  # PC2
-            '192.168.1.12': 3,  # PC3
-            '192.168.1.13': 4,  # PC4
-            '192.168.1.50': 5,  # WEB_SERVER
-            '192.168.1.51': 6,  # DB_SERVER
-            '192.168.1.254': 7  # INTERNET_PC
-        }
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
@@ -31,60 +36,24 @@ class SimpleSwitch13(app_manager.RyuApp):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
-        self.add_default_flows(datapath)
-
-    def add_default_flows(self, datapath):
-        ofproto = datapath.ofproto
-        parser = datapath.ofproto_parser
-
-        # Rule: Web server has full access to DB server
-        match = parser.OFPMatch(eth_type=ether_types.ETH_TYPE_IP,
-                                ipv4_src='192.168.1.50',
-                                ipv4_dst='192.168.1.51')
-        actions = [parser.OFPActionOutput(self.ip_to_port['192.168.1.51'])]
-        self.add_flow(datapath, 10, match, actions)
-
-        # Rule: Full access from site 1 and site 2 PCs to Web Server
-        for src_ip in ['192.168.1.10', '192.168.1.11', '192.168.1.12', '192.168.1.13']:
-            match = parser.OFPMatch(eth_type=ether_types.ETH_TYPE_IP,
-                                    ipv4_src=src_ip,
-                                    ipv4_dst='192.168.1.50')
-            actions = [parser.OFPActionOutput(self.ip_to_port['192.168.1.50'])]
-            self.add_flow(datapath, 10, match, actions)
-
-        # Rule: Block access from site 1 and site 2 PCs to DB server
-        for src_ip in ['192.168.1.10', '192.168.1.11', '192.168.1.12', '192.168.1.13']:
-            match = parser.OFPMatch(eth_type=ether_types.ETH_TYPE_IP,
-                                    ipv4_src=src_ip,
-                                    ipv4_dst='192.168.1.51')
-            actions = []  # No actions -> Drop the packet
-            self.add_flow(datapath, 10, match, actions)
-
-        # Rule: Internet PC can only access Web Server for HTTP (port 80)
-        match = parser.OFPMatch(eth_type=ether_types.ETH_TYPE_IP,
-                                ipv4_src='192.168.1.254',
-                                ipv4_dst='192.168.1.50',
-                                ip_proto=6,  # TCP
-                                tcp_dst=80)
-        actions = [parser.OFPActionOutput(self.ip_to_port['192.168.1.50'])]
-        self.add_flow(datapath, 10, match, actions)
-
-        # Rule: Block all other traffic from Internet PC
-        match = parser.OFPMatch(eth_type=ether_types.ETH_TYPE_IP,
-                                ipv4_src='192.168.1.254')
-        actions = []  # No actions -> Drop the packet
-        self.add_flow(datapath, 10, match, actions)
-
-        # Allow ARP packets
-        match = parser.OFPMatch(eth_type=ether_types.ETH_TYPE_ARP)
-        actions = [parser.OFPActionOutput(ofproto.OFPP_FLOOD)]
+        # install table-miss flow entry
+        #
+        # We specify NO BUFFER to max_len of the output action due to
+        # OVS bug. At this moment, if we specify a lesser number, e.g.,
+        # 128, OVS will send Packet-In with invalid buffer_id and
+        # truncated packet data. In that case, we cannot output packets
+        # correctly.  The bug has been fixed in OVS v2.1.0.
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                          ofproto.OFPCML_NO_BUFFER)]
         self.add_flow(datapath, 0, match, actions)
 
     def add_flow(self, datapath, priority, match, actions, buffer_id=None):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
-        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                             actions)]
         if buffer_id:
             mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
                                     priority=priority, match=match,
@@ -94,54 +63,89 @@ class SimpleSwitch13(app_manager.RyuApp):
                                     match=match, instructions=inst)
         datapath.send_msg(mod)
 
+
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
+        # If you hit this you might want to increase
+        # the "miss_send_length" of your switch
+       
+        if ev.msg.msg_len < ev.msg.total_len:
+            self.logger.debug("packet truncated: only %s of %s bytes",
+                              ev.msg.msg_len, ev.msg.total_len)
         msg = ev.msg
         datapath = msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
+        dpid = datapath.id
         in_port = msg.match['in_port']
-
         pkt = packet.Packet(msg.data)
         eth = pkt.get_protocols(ethernet.ethernet)[0]
-
-        # Ignore LLDP packets
-        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
-            return
-
+        ippkt = pkt.__contains__(ipv4.ipv4) # if packet contains IP
+        tcppkt = pkt.__contains__(tcp.tcp) # if packet contains tcppkt
+        udppkt = pkt.__contains__(udp.udp) # if packet contains udppkt
+        dst_mac = eth.dst
+        src_mac = eth.src
+        self.mac_to_port.setdefault(dpid, {})
+        # learn a mac address to avoid FLOOD next time.
         dst = eth.dst
         src = eth.src
-
-        dpid = datapath.id
-        self.mac_to_port.setdefault(dpid, {})
+        self.mac_to_port[dpid][src] = in_port
 
         self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
 
-        # Learn a mac address to avoid FLOOD next time.
-        self.mac_to_port[dpid][src] = in_port
+        # match criteria
+        # basic match
 
-        # Install a flow to avoid packet_in next time
-        if dst in self.mac_to_port[dpid]:
-            out_port = self.mac_to_port[dpid][dst]
+        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
+            # process LLDP packet
+            return
+
+        elif ippkt: # extended matches
+            print("in ip")
+            ipp = pkt.get_protocols(ipv4.ipv4)[0]
+            if tcppkt:
+                print("in tcp")
+                tcpp = pkt.get_protocols(tcp.tcp)[0]
+                match = parser.OFPMatch(eth_dst=dst_mac,eth_src=src_mac,eth_type=ether_types.ETH_TYPE_IP,
+                                        ip_proto=ipp.proto,ipv4_dst=ipp.dst,ipv4_src=ipp.src,
+                                        tcp_src=tcpp.src_port,tcp_dst=tcpp.dst_port)
+
+            elif udppkt:
+                udpp = pkt.get_protocols(udp.udp)[0]
+                match = parser.OFPMatch(eth_dst=dst_mac,eth_src=src_mac,eth_type=ether_types.ETH_TYPE_IP,
+                                        ip_proto=ipp.proto,ipv4_dst=ipp.dst,ipv4_src=ipp.src,
+                                        udp_src=udpp.src_port,udp_dst=udpp.dst_port)
+            else:
+                match = parser.OFPMatch(eth_dst=dst_mac,eth_src=src_mac,eth_type=ether_types.ETH_TYPE_IP,
+                                        ip_proto=ipp.proto,ipv4_dst=ipp.dst,ipv4_src=ipp.src)
         else:
-            out_port = ofproto.OFPP_FLOOD
+             match = parser.OFPMatch(eth_dst=dst_mac,eth_src=src_mac,eth_type=eth.ethertype)
+        #actions
+           
+        # -------------------drop packet from internet pc to db server -----------------
+        if ippkt and ipp.src=="192.168.1.254" and ipp.dst=="192.168.1.51":
+            out_port=''
+            actions = []
+        
+        else: # if not blocking port, just allow based on mac address else, flood to discover
+            if dst in self.mac_to_port[dpid]:
+                out_port = self.mac_to_port[dpid][dst]
+            else:
+                out_port = ofproto.OFPP_FLOOD
+            actions = [parser.OFPActionOutput(out_port)]
 
-        actions = [parser.OFPActionOutput(out_port)]
 
-        # Install a flow to avoid packet_in next time
+        # install a flow to avoid packet_in next time
         if out_port != ofproto.OFPP_FLOOD:
-            match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
-            # Verify if we have a valid buffer_id, if yes avoid to send both
-            # flow_mod & packet_out
             if msg.buffer_id != ofproto.OFP_NO_BUFFER:
                 self.add_flow(datapath, 1, match, actions, msg.buffer_id)
                 return
             else:
                 self.add_flow(datapath, 1, match, actions)
-        data = None
-        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
-            data = msg.data
-
-        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+        else: # incase its flood pkt
+            data = None
+            if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+                data = msg.data
+            out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
                                   in_port=in_port, actions=actions, data=data)
-        datapath.send_msg(out)
+            datapath.send_msg(out)
